1838aa32d021faf5a1b97727e301277f0abedb5c
jessi ortega <ortegabx05@gmail.com>
2019-07-21 22:14:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,22 +66,11 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+ 		{:grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"]},
+     :alan_kay => {:known_for => "Object Orientation":languages => ["Smalltalk", "LISP"]},
+     :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]}}
+   
    programmer_hash[:dennis_ritchie][:languages][0]
-    
 end
 
 def changing_alan
